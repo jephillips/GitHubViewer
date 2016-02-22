b07679978b854f175a8fc00f5cc1045bdb1ab046
Josh <jephillips@cohesionforce.com>
2016-02-21 19:18:19 -0600

    Added link to github page from Repo list

--- app/components/Repositories.js
+++ app/components/Repositories.js
@@ -1,6 +1,7 @@
 var React = require('react-native');
 var Badge = require('./Common/Badge');
 var Seperator = require('./Common/Seperator');
+var WebView = require('./Common/WebView');
 
 var {
   Text,
@@ -37,7 +38,11 @@ var styles = StyleSheet.create({
 
 class Repositories extends React.Component {
   openPage(url){
-    console.log(url);
+    this.props.navigator.push({
+      component: WebView,
+      title: 'Web View',
+      passProps: {url}
+    })
   }
   render(){
     var repos = this.props.repos;
