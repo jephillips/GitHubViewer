b628f9b3c9c56848283f4873aad4cb3aab60f248
Josh <jephillips@cohesionforce.com>
2016-02-22 20:26:03 -0600

    Fixed issue in path construction for Firebase

--- app/services/DBService.js
+++ app/services/DBService.js
@@ -1,13 +1,13 @@
 var DBService = {
   getNotes(username){
     username = username.toLowerCase().trim();
-    var url = "https://jpgithubviewer.firebaseio.com/${username}.json";
+    var url = `https://jpgithubviewer.firebaseio.com/${username}.json`;
     return fetch(url)
       .then((res) => res.json());
   },
   addNote(username, note){
     username = username.toLowerCase().trim();
-    var url = "https://jpgithubviewer.firebaseio.com/${username}.json";
+    var url = `https://jpgithubviewer.firebaseio.com/${username}.json`;
     return fetch(url, {
       method: 'post',
       body: JSON.stringify(note)
